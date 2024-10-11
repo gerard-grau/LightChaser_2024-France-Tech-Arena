be264e369e6544b21cc01b28aaa962f0e9772cde
gerardgrau <gerardgraugarcia@gmail.com>
2024-10-11 17:34:57 +0200

    Add new ReadInput namespace

--- main.cpp
+++ main.cpp
@@ -30,43 +30,43 @@ struct Input {
 
 Input input;
 
+namespace ReadInput {
+    void read_input() {
+        cin >> input.N >> input.M;
+        input.P.resize(input.N);
+        input.edges.resize(input.M);
 
-void read_input() {
-    cin >> input.N >> input.M;
-    input.P.resize(input.N);
-    input.edges.resize(input.M);
+        for (int& Pi : input.P) {
+            cin >> Pi;
+        }
+        for (Edge& edge : input.edges) {
+            int ui, vi;
+            cin >> ui >> vi;
+            edge.u = ui - 1;
+            edge.v = vi - 1;
+        }
 
-    for (int& Pi : input.P) {
-        cin >> Pi;
-    }
-    for (Edge& edge : input.edges) {
-        int ui, vi;
-        cin >> ui >> vi;
-        edge.u = ui - 1;
-        edge.v = vi - 1;
-    }
+        cin >> input.J;
+        input.services.resize(input.J);
 
-    cin >> input.J;
-    input.services.resize(input.J);
+        for (int i = 0; i < input.J; i++) {
+            Service& service = input.services[i];
+            int s, d, L, R;
+            cin >> s >> d >> service.S >> L >> R >> service.V;
+            service.s = s - 1;
+            service.d = d - 1;
+            service.L = L - 1;
+            service.R = R - 1;
 
-    for (int i = 0; i < input.J; i++) {
-        Service& service = input.services[i];
-        int s, d, L, R;
-        cin >> s >> d >> service.S >> L >> R >> service.V;
-        service.s = s - 1;
-        service.d = d - 1;
-        service.L = L - 1;
-        service.R = R - 1;
-
-        service.edges.resize(service.S);
-        for (Edge& edge : service.edges) {
-            int ej;
-            cin >> ej;
-            edge = input.edges[ej-1];
+            service.edges.resize(service.S);
+            for (Edge& edge : service.edges) {
+                int ej;
+                cin >> ej;
+                edge = input.edges[ej-1];
+            }
         }
     }
 }
-
 
 namespace Bottleneck {
 
@@ -88,7 +88,7 @@ namespace Bottleneck {
 
     }
 
-    void Bottleneck() {
+    void bottleneck() {
         
         print_bottleneck_results();
     }
@@ -97,18 +97,18 @@ namespace Bottleneck {
 
 namespace Replanning {
 
-    void Replanning() {
+    void replanning() {
         
     }
 }
 
 
 int main() {
-    read_input();
+    ReadInput::read_input();
     
-    Bottleneck::Bottleneck();
+    Bottleneck::bottleneck();
     
-    Replanning::Replanning();
+    Replanning::replanning();
 
     return 0;
 }
